b39c0ff12d2079e3c0dc9dfcbf4c5a7d886f24c6
bharath-9629688 <sabariammu280@gmail.com>
2025-07-21 12:22:42 +0530

    Create contact.jsx

---
+++ contact.jsx
@@ -0,0 +1,23 @@
+
+
+export default function Contact() {
+  return (
+     <section>
+      <div className="max-w-[55rem] w-full mx-auto px-4">
+        <div className="grid grid-cols-1">
+          <div className="mb-5">
+            <h1 className="text-3xl font-bold mb-3">Contact-US</h1>
+            <p className="mb-3 text-red-500"> that can be made for all developer to use full for the info</p>
+             <p className="text-[19px] text-brown-400">We're happy to share the release of Vite 7! It has been 5 years since Evan You sent the first commit to the Vite repo, and nobody could have pr
+edicted how much the frontend ecosystem would change since then Through a partnership between VoidZero and NuxtLabs, Anthony Fu is working on creating Vite DevTools. They will offer deeper and more insightful debugging and analysis for all Vite-based projects and frameworks. You can read more on the VoidZero and NuxtLabs join forces on Vite Devtools blog post.
+Through a partnership between VoidZero and NuxtLabs, Anthony Fu is working on creating Vite DevTools. They will offer deeper and more insightful debugging and analysis for all Vite-based projects and frameworks. You can read more on the VoidZero and NuxtLabs join forces on Vite Devtools blog post.</p> 
+             
+            </div>
+
+            
+        </div>
+
+      </div>
+    </section>
+  )
+}

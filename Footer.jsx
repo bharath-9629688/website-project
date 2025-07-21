41822b632fff9bf8fa0131585808780ea5ad5d9d
bharath-9629688 <sabariammu280@gmail.com>
2025-07-21 12:24:01 +0530

    Create Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer>
+        <div className="w-full max-w-full px-4 max-auto">
+            <div className="flex justify-center text-red-500 hover:text-black">
+                        <div></div>
+            </div>
+        </div>
+        </footer>
+    );
+}
+
+export default Footer;

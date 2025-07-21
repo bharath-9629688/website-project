7e6a44e5228aa866c4e913244fee4ba90ff4ffd9
bharath-9629688 <sabariammu280@gmail.com>
2025-07-21 12:26:37 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+
+import Approuter from './assets/core/Router/Approuter'
+
+const App = () => {
+  return (
+    <div>
+   
+      
+       
+        <Approuter/>
+     
+      </div>
+  )      
+}
+
+export default App
